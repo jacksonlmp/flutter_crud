9a89b9e5914433b4c33141288824b2a64835b938
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-12-20 19:43:08 -0300

    User class and fix datas

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  ),
+   '2': User(
+    id: '2',
+    name: 'Marcos',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  ),
+   '3': User(
+    id: '3',
+    name: 'Davi',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  ),
+   '4': User(
+    id: '4',
+    name: 'Thiago',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  ),
+   '5': User(
+    id: '5',
+    name: 'Mario',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  )
+};
